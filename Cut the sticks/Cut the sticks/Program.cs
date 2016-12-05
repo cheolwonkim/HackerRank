0526c7d369d36e1d09fcf911529a51843a4dd43c
CHEOLWONKIM9FBC\cheolwon <cheolwon.kim@h9works.com>
2016-12-05 18:09:18 +0900

    https://www.hackerrank.com/challenges/cut-the-sticks/submissions/code/24784112

---
+++ Cut the sticks/Cut the sticks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cut_the_Sticks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] sticks = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            int count = 0;
+            int min;
+
+            while (true)
+            {
+                min = int.MaxValue;
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (sticks[j] != 0)
+                        {
+                            min = sticks[j];
+                            break;
+                        }
+                    }
+                }
+                for (int i = 0; i < N; i++)
+                {
+                    if (min > sticks[i] && sticks[i] != 0)
+                    {
+                        min = sticks[i];
+                    }
+                }
+
+                for (int i = 0; i < N; i++)
+                {
+                    if (sticks[i] > 0)
+                    {
+                        sticks[i] -= min;
+                        count++;
+                    }
+                }
+                if (count == 0)
+                {
+                    break;
+                }
+                Console.WriteLine(count);
+
+                count = 0;
+            }
+
+        }
+    }
+}
