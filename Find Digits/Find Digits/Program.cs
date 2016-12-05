4ed2c4ff72d73fcd33fb46bfd42f956e2f199419
CHEOLWONKIM9FBC\cheolwon <cheolwon.kim@h9works.com>
2016-12-05 18:11:41 +0900

    https://www.hackerrank.com/challenges/find-digits

---
+++ Find Digits/Find Digits/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int MAX = 1000000000;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int remainderN = n;
+            int digitN = MAX; ;
+            int di;
+            int count = 0;
+            while (digitN > n)
+            {
+                digitN /= 10;
+            }
+
+            while (digitN >= 1)
+            {
+                di = remainderN / digitN;
+                if ((di != 0) && ((n % di) == 0))
+                {
+                    count++;
+                }
+                remainderN = remainderN % digitN;
+                digitN /= 10;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
+
