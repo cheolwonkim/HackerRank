1d4f503a3285501ce510faac87a6f95d0af6e8ec
CHEOLWONKIM9FBC\cheolwon <cheolwon.kim@h9works.com>
2016-12-05 18:05:37 +0900

    https://www.hackerrank.com/challenges/encryption/copy-from/24329913

---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string readString = Console.ReadLine();
+            string removeSpaceString = readString.Replace(" ", "");
+
+            int temp = (int)Math.Sqrt(removeSpaceString.Length);
+            int floor, ceil;
+
+            if ((temp * (temp + 1)) < removeSpaceString.Length)
+                floor = temp + 1;
+            else
+                floor = temp;
+
+            temp = floor;
+            if ((temp * temp) >= removeSpaceString.Length)
+                ceil = temp;
+            else
+                ceil = temp + 1;
+
+
+            int i, j;
+            for (j = 0; j < ceil; j++)
+            {
+                for (i = 0; i < floor; i++)
+                {
+                    if ((ceil * i + j) >= (removeSpaceString.Length))
+                        break;
+
+                    Console.Write(removeSpaceString[ceil * i + j]);
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
